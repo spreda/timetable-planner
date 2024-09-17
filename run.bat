134a27acbad283d79c563dbc6e8f1fb8aec04b0a
Kirill Kamenev <ctktea@gmail.com>
2024-09-17 20:15:46 +0500

    Исправил скрипты для запуска

--- run.bat
+++ run.bat
@@ -1,7 +1,9 @@
 if not exist .venv (
-    python -m venv .venv
+    python -m venv .venv 
+    call ./.venv/Scripts/activate
     pip install -r requirements.txt
+) else (
+    call ./.venv/Scripts/activate
 )
-call ./.venv/Scripts/activate
 cd server
 uvicorn main:app --reload
--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 if [ -d ".venv" ]; then
     python -m venv .venv
+    ./.venv/Scripts/activate
     pip install -r requirements.txt
+else
+    ./.venv/Scripts/activate
 fi
-./.venv/Scripts/activate
 cd server
 uvicorn main:app --reload
